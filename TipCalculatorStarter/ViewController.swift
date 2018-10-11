018295a26b8129f44f641288d1a8ca5f85911bf2
Sujit Molleti <sujit.molleti@rutgers.edu>
2018-10-11 12:01:33 -0400

    Initial commit

---
+++ TipCalculatorStarter/ViewController.swift
@@ -0,0 +1,154 @@
+//
+//  ViewController.swift
+//  TipCalculatorStarter
+//
+//  Created by Chase Wang on 9/19/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // 1
+    var isDefaultStatusBar = true
+    
+    // 2
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return isDefaultStatusBar ? .default : .lightContent
+    }
+
+    @IBOutlet weak var headerView: UIView!
+    // MARK: - View Lifecycle
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: BillAmountTextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setTheme(isDark: false)
+        
+        setupViews()
+        
+        billAmountTextField.calculateButtonAction = {
+            self.calculate()
+        }
+    }
+    func setTheme(isDark: Bool) {
+        let theme = isDark ? ColorTheme.dark : ColorTheme.light
+        
+        view.backgroundColor = theme.viewControllerBackgroundColor
+        
+        headerView.backgroundColor = theme.primaryColor
+        titleLabel.textColor = theme.primaryTextColor
+        
+        inputCardView.backgroundColor = theme.secondaryColor
+        
+        billAmountTextField.tintColor = theme.accentColor
+        tipPercentSegmentedControl.tintColor = theme.accentColor
+        
+        outputCardView.backgroundColor = theme.primaryColor
+        outputCardView.layer.borderColor = theme.accentColor.cgColor
+        
+        tipAmountTitleLabel.textColor = theme.primaryTextColor
+        totalAmountTitleLabel.textColor = theme.primaryTextColor
+        
+        tipAmountLabel.textColor = theme.outputTextColor
+        totalAmountLabel.textColor = theme.outputTextColor
+        
+        resetButton.backgroundColor = theme.secondaryColor
+        
+        isDefaultStatusBar = theme.isDefaultStatusBar
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    func setupViews(){
+        headerView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        headerView.layer.shadowOpacity = 0.05
+        headerView.layer.shadowColor = UIColor.black.cgColor
+        headerView.layer.shadowRadius = 35
+        
+        inputCardView.layer.cornerRadius = 8
+        inputCardView.layer.masksToBounds = true
+        
+        outputCardView.layer.cornerRadius = 8
+        outputCardView.layer.masksToBounds = true
+        
+        // set output card border
+        outputCardView.layer.borderWidth = 1
+        outputCardView.layer.borderColor = UIColor.tcHotPink.cgColor
+        
+        resetButton.layer.cornerRadius = 8
+        resetButton.layer.masksToBounds = true
+    }
+    
+    func clear() {
+        billAmountTextField.text = nil
+        tipPercentSegmentedControl.selectedSegmentIndex = 0
+        tipAmountLabel.text = "$0.00"
+        totalAmountLabel.text = "$0.00"
+    }
+    
+    func calculate() {
+        // dismiss keyboard
+        if self.billAmountTextField.isFirstResponder {
+            self.billAmountTextField.resignFirstResponder()
+        }
+        
+        guard let billAmountText = self.billAmountTextField.text,
+            let billAmount = Double(billAmountText) else {
+                clear()
+                return
+        }
+        
+        let roundedBillAmount = (100 * billAmount).rounded() / 100
+        
+        let tipPercent: Double
+        switch tipPercentSegmentedControl.selectedSegmentIndex {
+        case 0:
+            tipPercent = 0.15
+        case 1:
+            tipPercent = 0.18
+        case 2:
+            tipPercent = 0.20
+        default:
+            preconditionFailure("Unexpected index.")
+        }
+        
+        let tipAmount = roundedBillAmount * tipPercent
+        let roundedTipAmount = (100 * tipAmount).rounded() / 100
+        
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        // Update UI
+        self.billAmountTextField.text = String(format: "%.2f", roundedBillAmount)
+        self.tipAmountLabel.text = String(format: "%.2f", roundedTipAmount)
+        self.totalAmountLabel.text = String(format: "%.2f", totalAmount)
+    }
+    
+    @IBAction func themeToggled(_ sender: Any) {
+        setTheme(isDark: (sender as AnyObject).isOn)
+    }
+    @IBAction func tipPercentChanged(_ sender: Any) {
+        calculate()
+    }
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        clear()
+    }
+}
+
